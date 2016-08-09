16aeec6ffb2867d7a69ee6911a7ede9d6e3d3d72
Alex Dillhoff <orbv16@gmail.com>
2016-08-08 22:15:37 -0500

    Corrected the split set function. It should also split based on the sequence index for a sample as described in Definition 2.2

--- tests/split_criteria_test.m
+++ tests/split_criteria_test.m
@@ -3,9 +3,11 @@
 % Alex Dillhoff
 %%
 
+D = 3;
+
 W_pos = rand(5, 1);
 W_neg = rand(5, 1);
-Y_pos = randi(3, 5, 1);
-Y_neg = randi(3, 5, 1);
+Y_pos = randi(D, 5, 1);
+Y_neg = randi(D, 5, 1);
 
 calculate_split_criteria(W_pos, W_neg, Y_pos, Y_neg)
--- utils/split_set.m
+++ utils/split_set.m
@@ -3,15 +3,20 @@
 % Alex Dillhoff
 %%
 
-function [X_pos, X_neg] = split_set(X, d)
+function [X_pos, X_neg] = split_set(X, l, d)
     %%
-    % split_set(X, d)
+    % split_set(X, l, d)
     %
-    % Splits the dataset given the dimension d. The set `X_pos` will contain
-    % all samples such that the feature at dimension `d` is 1. The set `X_neg`
-    % will contain the rest.
+    % Splits the dataset given the dimension d and the sequence index l. The set
+    % `X_pos` will contain all samples such that the feature at dimension `d` is
+    % 1. The set `X_neg` will contain the rest.
     %%
 
-    [X_pos_ind, c] = find(X(:, :, d) == 1)
-    [X_neg_ind, c] = find(X(:, :, d) == 0)
+    [X_pos_row, X_pos_col] = ind2sub(size(X), find(X(:, l, d) == 1));
+
+    X_pos_row = unique(X_pos_row);
+    X_neg_row = find(~ismember(1:size(X, 1), X_pos_row));
+
+    X_pos = X(X_pos_row, :, :);
+    X_neg = X(X_neg_row, :, :);
 end
